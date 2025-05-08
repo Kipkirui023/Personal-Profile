fe112d7fa5e89321eb779b0500b81378e93ff1f3
Kipkurui Laban Langat <kipkiruilaban02003@gmail.com>
2025-05-08 18:17:47 +0300

    Pushing the source code Github to submit my Attarchment Assignment

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".namet", {
+      strings: ["KIPKURUI", "LANGAT", "LABAN"],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1000,
+      loop: true
+    });
+  });
